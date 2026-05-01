60e3a596e2c5ec45f3aa951fd7f5dc41f95fbdc6
PrachiKhandelwal <prachik27200001@gmail.com>
2026-05-02 02:25:32 +0530

    create reusable prompt template

--- 4.prompts/prompt_ui.py
+++ 4.prompts/prompt_ui.py
@@ -1,32 +1,10 @@
 import streamlit as st
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import load_prompt
 from dotenv import load_dotenv
 
 load_dotenv()
 
-template = PromptTemplate(template='''
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-
-1. Mathematical Details:
-
-   * Include relevant mathematical equations if present in the paper.
-   * Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-
-2. Analogies:
-
-   * Use relatable analogies to simplify complex ideas.
-
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-
-Ensure the summary is clear, accurate, and aligned with the provided style and length. End with a complete concluding sentence. Do not stop mid-sentence. If you are running out of space, wrap up early rather than cutting off.
-
-''', 
-input_variables=['paper_input','style_input','length_input'],
-validate_template=True
-)
 
 st.header('Research App')
 
@@ -35,6 +13,8 @@ research_paper = st.selectbox('Select Research Paper',['Attention Is All You Nee
 summary_style = st.selectbox('Select style',['Code oriented','Maths Oriented', 'Beginner', 'Academic'])
 
 summary_length = st.selectbox('Select length',['Short (1-2 paragraphs)','Long (5-6 paragraphs)','Medium(3-4 paragraphs)'])
+
+template = load_prompt('template.json')
 
 prompt = template.invoke({
     'paper_input':research_paper,
